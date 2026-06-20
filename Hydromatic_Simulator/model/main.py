7770eda89d2c898ac9c3b9e2d54d08279b54a7bb
Soft Active Materials Lab <yejin.shim12@gmail.com>
2026-06-20 13:15:03 -0500

    Update main.py

--- Hydromatic_Simulator/model/main.py
+++ Hydromatic_Simulator/model/main.py
@@ -8,8 +8,8 @@ from Hydromatic_Simulator.model.model import GeneratorModel
 from Hydromatic_Simulator.utils.config import config
 from Hydromatic_Simulator.model.module import EncodingModule, DecodingModule
 
-def load_trained_model(data_path="./Hydromatic_Simulator/dataset",
-                       weights_path="./Hydromatic_Simulator/model/weights"):
+def load_trained_model(data_path=os.path.join(".", "Hydromatic_Simulator", "dataset"),
+                       weights_path=os.path.join(".", "Hydromatic_Simulator", "model", "weights")):
 
   test_data = import_data(data_path)
 
@@ -37,7 +37,7 @@ def load_trained_model(data_path="./Hydromatic_Simulator/dataset",
 
 
 def import_data(data_path):
-    with open(data_path + '/test_data.pkl', 'rb') as f:
+    with open(os.path.join(data_path, 'test_data.pkl'), 'rb') as f:
         test_data = pickle.load(f)
         
     return test_data
@@ -48,5 +48,4 @@ def make_dataset(batch_size, design_idx, design_input, true_nodal_x, true_nodal_
     tf3 = tf.stack(
         [tf.convert_to_tensor(true_nodal_x), tf.convert_to_tensor(true_nodal_y)], axis=-1)
     dataset = tf.data.Dataset.from_tensor_slices((tf1, tf2, tf3))
-    # DO NOT RANDOMIZE: keep X and Y coords aligned
     return dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
