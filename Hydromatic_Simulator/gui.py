dfeba9e6d062e7686d0a8e21abfad9f7b1c3999f
Soft Active Materials Lab <yejin.shim12@gmail.com>
2026-06-20 13:14:26 -0500

    Update gui.py

--- Hydromatic_Simulator/gui.py
+++ Hydromatic_Simulator/gui.py
@@ -1,6 +1,7 @@
+import os
+import sys
 import tkinter as tk
 from tkinter import PhotoImage
-import sys
 
 from Hydromatic_Simulator.ui.ui_app import BinaryPredictorApp
 from Hydromatic_Simulator.utils import mac_icon
@@ -8,10 +9,17 @@ from Hydromatic_Simulator.utils import mac_icon
 def run_gui():
     root = tk.Tk()
     root.title("Hydromatic Simulator")
+    
+    icon_path = os.path.join(".", "Hydromatic_Simulator", "icon.png")
+
     if sys.platform == "darwin":
-        mac_icon.set_dock_icon("./Hydromatic_Simulator/icon.png")
+        mac_icon.set_dock_icon(icon_path)
         
-    icon = PhotoImage(file='./Hydromatic_Simulator/icon.png')
-    root.iconphoto(True, icon)
+    try:
+        icon = PhotoImage(file=icon_path)
+        root.iconphoto(True, icon)
+    except tk.TclError:
+        print(f"Notice: Icon is only loadable in Mac environment. Using default.")
+
     app = BinaryPredictorApp(root)
     root.mainloop()
